96b586f3e66c80cbab29d78b3e409f8e55c9f231
Jordan Smith <jordan@jsmith.me>
2013-07-09 19:58:10 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Futurecaller::Application.config.secret_token = '29d04629158fb8d96b6f2afa04269a31c98cf1406d4b9f6036f2716db057597e1cfbfbbe073edb8401603804f225be10c55127a8c0d3adcda3a0a334dc73f6bd'
