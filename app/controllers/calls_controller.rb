097cc3fa06c1801c5f77abc18e869a8d9605ecf0
Frank Gal <frankgaldrinks@gmail.com>
2013-07-10 16:32:23 -0400

    Fixed date time and style more

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class CallsController < ApplicationController
   def index
   	@call = Call.new
@@ -8,14 +10,8 @@ class CallsController < ApplicationController
   	@call.num_to_call = params[:call][:num_to_call]
   	@call.caller_id = params[:call][:caller_id]
   	@call.text_to_say = params[:call][:text_to_say]
-  	@call.time_to_call = ""
-  	params[:time_to_call].each do |key,value|
-  		@call.time_to_call << "#{value}"
-  		if key != "minute"
-  			@call.time_to_call << ":"
-  		end	
-  	end
-
+    @call.time_to_call = setdateformat(params[:time_to_call])
+     
 
   	if @call.save
   		render 'success'
@@ -27,4 +23,19 @@ class CallsController < ApplicationController
   def success
   	
   end
+
+  private
+
+    def setdateformat(thetime)
+      date_param = thetime
+      if date_param['month'].length == 1
+        date_param['month'] = "0#{date_param['month']}"
+      end
+      if date_param['day'].length == 1
+        date_param['day'] = "0#{date_param['day']}"
+      end
+      date_format = "#{date_param['month']}/#{date_param['day']}/#{date_param['year']}"
+      time_format = "#{date_param['hour']}:#{date_param['minute']}"
+      return "#{date_format} - #{time_format}"
+    end
 end
